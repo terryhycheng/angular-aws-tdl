64d33fb471841224391943b251ea1c61acaf4386
Terry Cheng <terryhycheng@gmail.com>
2022-07-26 08:08:40 +0800

    initial commit

---
+++ backend/handler.js
@@ -0,0 +1,25 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+export default app;
+
+module.exports.handler = serverless(app);
