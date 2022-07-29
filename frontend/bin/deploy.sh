4a069cfece0dcd6d2bee5c6b5e68393106bc7be9
terryhycheng <terryhycheng@gmail.com>
2022-07-29 17:32:26 +0800

    deploy.sh updated.

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./dist s3://angular-aws-tdl
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://angular-aws-tdl
+aws s3 cp --recursive --acl public-read ./dist/front s3://angular-aws-tdl/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/front/index.html s3://angular-aws-tdl/
